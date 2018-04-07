752056a5d343bb4a9664b729d8a6ee21c10d9210
Manav <manavk123@gmail.com>
2018-04-07 11:59:50 -0700

    normalize cca reduced vectors

--- mnist_cca.py
+++ mnist_cca.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from sklearn.cross_decomposition import CCA
+import numpy as np
 
 def get_train_data(one_hot=False):
     """ Fetches and returns training data and labels from MNIST data set.
@@ -8,8 +9,8 @@ def get_train_data(one_hot=False):
     Args:
         one_hot: whether to generate one-hot labels
     Returns:
-        train_data: (50000, 784) matrix
-        train_labels: (50000, 9) matrix (labels are one-hot)
+        train_data: (55000, 784) matrix
+        train_labels: (55000, 9) matrix (labels are one-hot)
     """
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=one_hot)
     train_data = mnist.train.images
@@ -17,8 +18,44 @@ def get_train_data(one_hot=False):
     return train_data, train_labels
 
 def CCA_reduction():
-    data, labels = get_train_data(one_hot=True)
-    X_scores, Y_scores = CCA(n_components=16).fit_transform(data, labels)
+    """ Reduces dimensions of MNIST data using CCA
 
-if __name__ == "__main__":
-    CCA_reduction()
+    Returns:
+        X_scores: training data reduced to 16 dimenional vectors
+    """
+    data, labels = get_train_data(one_hot=True)
+    X_scores, _ = CCA(n_components=16).fit_transform(data, labels)
+    return X_scores
+
+def normalize_row(row):
+    """ Normalizes data between 0 and 1.
+
+    Args:
+        row: list of values
+    Returns:
+        lst: list of values normalized between 0 and 1
+    """
+    max_val = max(row)
+    min_val = min(row)
+    diff = max_val - min_val
+    return [np.float(x - min_val)/np.float(diff) for x in row]
+
+def normalize_scores(data):
+    """ Normalizes matrix.
+
+    Args:
+        data: matrix to normalize
+    Returns:
+        lst: matrix where each row is normalized between 0 and 1
+    """
+    return [normalize_row(row) for row in data]
+
+def test_normalize_scores():
+    """ Tests that rows are normalized correctly.
+    """
+    X_scores = CCA_reduction()
+    normalized = normalize_scores(X_scores)
+    for row in normalized:
+        assert max(row) <= 1.0
+        assert min(row) >= 0.0
+    print("Rows normalized correctly")
